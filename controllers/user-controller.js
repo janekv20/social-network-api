4d31d3b5c603a3c96ce0755862a8115af98552e3
longhorn <83248746+janekv20@users.noreply.github.com>
2021-09-13 22:12:13 -0500

    added function to create, update and remove users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -41,5 +41,51 @@ const userController = {
                 console.loge(err);
                 res.status(400).json(err);
             });
-    }
+    },
+    //create a user
+    createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+    //update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id.' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    //delete a user
+    deleteUser({ params },res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(deletedUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id.' });
+                return;
+            }
+            return Thought.deleteMany(
+                { username: { $in: deletedUserData.username } },
+            )
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id.' })
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        });
+    },
+    
 }
