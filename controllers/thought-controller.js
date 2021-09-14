264a1a5166ab5dae4eb8a8d91abbbc7a775e6fa0
longhorn <83248746+janekv20@users.noreply.github.com>
2021-09-13 20:02:44 -0500

    create,update,delete thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -38,4 +38,52 @@ const thoughtController = {
             res.status(400).json(err);
         })
     },
+    //create thought to user
+    createThought({ params, body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId},
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: ' No user found with this id.' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    //update thought by user id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            body,
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found for this id.' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => res.status(404).json(err));
+    },
+    //delete thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found for this id.' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .cath(err => res.json(err));
+    },
+
 }
